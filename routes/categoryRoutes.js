2d58f10b5c0ba49d0f63adeb5e1a45dd51ca2f5f
Deepak <smileydeepu070@gmail.com>
2025-12-02 14:35:10 +0530

    Updated backend code

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,17 +5,11 @@ const router = express.Router();
 
 // get all categories
 router.get("/", async (req, res) => {
-  const cats = await Category.find();
-  res.json(cats);
-});
-
-// add a new category
-router.post("/", async (req, res) => {
   try {
-    const cat = await Category.create({ name: req.body.name });
-    res.json(cat);
+    const categories = await Category.find();
+    res.json(categories);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ error: "Server error" });
   }
 });
 
