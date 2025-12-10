ef2ed63840c05943f02d60c500f60e1ccb90c30b
Deepak <smileydeepu070@gmail.com>
2025-12-10 12:19:08 +0530

    describe changes

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  desc: String,
-  price: Number,
-  mrp: Number,          // <<<< ADD THIS
-  img: String,
-  categoryId: mongoose.Schema.Types.ObjectId,
-  features: Array
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  desc: { type: String, required: true },
+  price: { type: Number, required: true },
+  mrp: { type: Number, required: true },
+  img: { type: String, required: true },
+  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Category" }
 });
 
+const Product = mongoose.model("Product", ProductSchema);
 
-export default mongoose.model("Product", ProductSchema);
+export default Product;
